48838d1e4c37f5eaff9a02e7c7175b89ea4d1f9a
Edward Spencer <epfspencer@yahoo.com>
2021-01-08 15:38:39 +0000

    Create listings model and display on index

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=1000)
+    current_price = models.IntegerField()
+
+    # TODO: ImageField
+    # photo = models.ImageField(upload_to=)
