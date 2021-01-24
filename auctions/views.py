84203c9857d7ab2bcd63d27051983490e3b7ec67
Edward Spencer <epfspencer@yahoo.com>
2021-01-24 21:48:37 +0000

    Clean up some unnecessary items

--- auctions/views.py
+++ auctions/views.py
@@ -3,7 +3,7 @@ import sys
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
